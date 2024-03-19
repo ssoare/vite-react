82aa73eb570b92360c9a1155e2dd5df8564e7333
Soare <soarenexus@gmail.com>
2024-03-19 22:44:48 +0100

    change logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
     return (
         <header>
             <nav className="fixed top-0 flex items-center justify-between w-full h-12 px-4">
-                <h1 className="text-xl font-semibold">LOGO</h1>
+                <h1 className='flex flex-col italic font-semibold'>El Rincón <span className='text-orange-500'>del Bocata</span></h1>
                 <ul className={`absolute left-0 top-0 flex flex-col items-center justify-center w-screen gap-4 text-3xl text-orange-500 transition bg-zinc-900 menu ${menu ? '' : 'translate-x-full'} h-svh`}>
                     <li className='px-4 py-2'><Link to="/" onClick={toggleMenu}>Inicio</Link> </li>
                     <li className='px-4 py-2'><Link to="/carta" onClick={toggleMenu}>Carta</Link> </li>

e7840b6656fd83a6d7cd460e18de7f1063a07d35
ssoare <154654902+ssoare@users.noreply.github.com>
2024-03-19 08:27:48 +0000

    another way around

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './css/index.css'
 import { BrowserRouter } from 'react-router-dom'
+import Images from './images/images'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>

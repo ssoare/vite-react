8aecd49c3c16c19f18da2cc026c095261a84a72d
Soare <soarenexus@gmail.com>
2024-03-20 21:10:51 +0100

    add icons to contact

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,28 +1,38 @@
 import '../css/contacto.css'
+import { IoLogoWhatsapp } from "react-icons/io5";
+import { HiOutlinePhone } from "react-icons/hi";
+import { IoMailOutline } from "react-icons/io5";
+import { HiOutlineMapPin } from "react-icons/hi2";
 
 function Contacto() {
     return(
         <>
             <section id='contacto'
                     className="grid items-center justify-center text-center 
-                            lg:gap-20 lg:mx-20 lg:grid lg:grid-cols-3">
+                            lg:gap-4 lg:mx-20 lg:grid lg:grid-cols-2">
                 <article className="border-b-2 py-12 mx-4 
                                     lg:border-y-2">
-                    <h2 className='text-3xl mb-4'>Llámanos: </h2>
-                    <a className="text-blue-500 p-4 text-lg font-semibold"
-                        href="tel:+34964964964">964 964 964</a>
-                </article>
-                <article className="border-b-2 py-12 mx-4 
-                                    lg:border-y-2">
-                    <h2 className='text-3xl mb-4'>Envíanos un correo:</h2>
-                    <a className="text-blue-500 p-4 text-lg font-semibold"
-                        href="mailto:correo@correo.com">correo@correo.com</a>
+                    <h2 className='text-3xl mb-4'>Teléfono</h2>
+                    <a className="text-blue-500 p-4 text-lg font-semibold flex items-center gap-4 justify-center"
+                        href="tel:+34964964964"><HiOutlinePhone className='size-6 text-black' /> 964 964 964</a>
                 </article>
                 <article className="py-12 mx-4 
                                     lg:border-y-2">
-                    <h2 className='text-3xl mb-4'>Estmos en:</h2>
-                    <a className="text-blue-500 p-4 text-lg font-semibold"
-                        href="https://maps.app.goo.gl/dLZjSYj1CSbLducL7">Pl. de Tetuan, 37, 12001 Castelló de la Plana, Castelló</a>
+                    <h2 className='text-3xl mb-4'>Whatsapp</h2>
+                    <a className="text-blue-500 p-4 text-lg font-semibold flex items-center gap-4 justify-center"
+                        href="whatsapp:+34964964964"><IoLogoWhatsapp className=' fill-green-500 size-6' /> 964 964 964</a>
+                </article>
+                <article className="border-b-2 py-12 mx-4 
+                                    lg:border-y-2">
+                    <h2 className='text-3xl mb-4'>Correo</h2>
+                    <a className="text-blue-500 p-4 text-lg font-semibold flex items-center gap-4 justify-center "
+                        href="mailto:correo@correo.com"> <IoMailOutline className='size-6 text-black' /> correo@correo.com</a>
+                </article>
+                <article className="py-12 mx-4 
+                                    lg:border-y-2">
+                    <h2 className='text-3xl mb-4'>Maps</h2>
+                    <a className="text-blue-500 p-4 text-lg font-semibold flex items-center gap-4 justify-center"
+                        href="https://maps.app.goo.gl/dLZjSYj1CSbLducL7"><HiOutlineMapPin className='size-6 text-black' />Pl. de Tetuan, 37, Castellón</a>
                 </article>
             </section>
         </>

ced7dd0fd25836e5be3283cb95ffc8d617f7b0fa
ssoare <154654902+ssoare@users.noreply.github.com>
2024-03-20 06:02:35 +0000

    cambiar tamaño text de los links de contacto

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -9,19 +9,19 @@ function Contacto() {
                 <article className="border-b-2 py-12 mx-4 
                                     lg:border-y-2">
                     <h2 className='text-3xl mb-4'>Llámanos: </h2>
-                    <a className="text-blue-500 p-4 font-semibold"
+                    <a className="text-blue-500 p-4 text-lg font-semibold"
                         href="tel:+34964964964">964 964 964</a>
                 </article>
                 <article className="border-b-2 py-12 mx-4 
                                     lg:border-y-2">
                     <h2 className='text-3xl mb-4'>Envíanos un correo:</h2>
-                    <a className="text-blue-500 p-4 font-semibold"
+                    <a className="text-blue-500 p-4 text-lg font-semibold"
                         href="mailto:correo@correo.com">correo@correo.com</a>
                 </article>
                 <article className="py-12 mx-4 
                                     lg:border-y-2">
                     <h2 className='text-3xl mb-4'>Estmos en:</h2>
-                    <a className="text-blue-500 p-4 font-semibold"
+                    <a className="text-blue-500 p-4 text-lg font-semibold"
                         href="https://maps.app.goo.gl/dLZjSYj1CSbLducL7">Pl. de Tetuan, 37, 12001 Castelló de la Plana, Castelló</a>
                 </article>
             </section>

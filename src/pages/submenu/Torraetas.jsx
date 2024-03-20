862b11f4938e003f94416a3e477d787639c4391d
Soare <soarenexus@gmail.com>
2024-03-20 18:59:31 +0100

    torraetas hecho con card

--- src/pages/submenu/Torraetas.jsx
+++ src/pages/submenu/Torraetas.jsx
@@ -1,27 +1,32 @@
-const torraeta = {
-    'Jamón serrano a la plancha y tomate': { id: 23 , description: '', price: 6.95 , image: '' },
-    'Sobrasada y queso': { id: 24 , description: '', price: 6.95 , image: '' },
-    'Anchoas, ajo y pimentón': { id: 25 , description: '', price: 6.95 , image: '' },
-    'Queso fresco y salmón': { id: 26 , description: '', price: 6.95 , image: '' },
-}
+import Card from "../../components/Card"
 
 function Torraetas() {
     return(
         <>
-            {Object.entries(torraeta).map(([name, details]) => (
-                <article className="border-2 w-[300px] h-[300px] p-4 rounded-lg shadow-lg flex flex-col justify-between 
-                hover:scale-105 hover:bg-gray-100 cursor-pointer transition-all">
-                    <img className="w-full h-120"
-                    src="src/images/burger.png" alt="imagen" />
-                    <div className="flex justify-between items-center flex-grow">
-                        <div className='basis-5/6'>
-                            <h3 className="text-lg font-semibold">{details.id}. {name}</h3>
-                            <p className="italic text-gray-600">{details.description}</p>
-                        </div>
-                        <p>{details.price.toFixed(2)}€</p>
-                    </div>
-                </article>   
-            ))}
+            <Card id = '23'
+                name = 'Jamón serrano a la plancha y tomate'
+                description = ''
+                price = '6.95'
+                image = 'src/images/burger.png'
+            />
+            <Card id = '24'
+                name = 'Sobrasada y queso'
+                description = ''
+                price = '6.95'
+                image = 'src/images/burger.png'
+            />
+            <Card id = '25'
+                name = 'Anchoas, ajo y pimentón'
+                description = ''
+                price = '6.95'
+                image = 'src/images/burger.png'
+            />
+            <Card id = '26'
+                name = 'Queso fresco y salmón'
+                description = ''
+                price = '6.95'
+                image = 'src/images/burger.png'
+            />
         </>
     )
 }

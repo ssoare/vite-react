95341dc57cb8d40d41092aac246f60f387fec35d
ssoare <154654902+ssoare@users.noreply.github.com>
2024-03-20 06:35:06 +0000

    hacer menu responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,25 @@ function Header() {
     const toggleMenu = () => { setMenu(!menu); };
 
     return (
-        <header>
-            <nav className="fixed top-0 flex items-center justify-between w-full h-12 px-4">
+        <header className='flex justify-center'>
+            <nav className="fixed top-0 flex items-center justify-between w-full h-12 px-4 lg:max-w-screen-xl">
                 <Link to="/">
                     <h1 className='flex flex-col italic font-semibold'>El Rincón <span className='text-orange-500'>del Bocata</span></h1>
                 </Link>
-                <ul className={`absolute left-0 top-0 flex flex-col items-center justify-center w-screen gap-4 text-3xl text-orange-500 transition bg-zinc-900 menu ${menu ? '' : 'translate-x-full'} h-svh`}>
-                    <li className='px-4 py-2'><Link to="/" onClick={toggleMenu}>Inicio</Link> </li>
-                    <li className='px-4 py-2'><Link to="/carta" onClick={toggleMenu}>Carta</Link> </li>
-                    <li className='px-4 py-2'><Link to="/contacto" onClick={toggleMenu}>Contacto</Link> </li>
+                <ul className={`menu absolute inset-0 flex flex-col items-center justify-center w-screen gap-4 text-3xl text-orange-500 transition bg-zinc-900 
+                                ${menu ? '' : 'translate-x-full'} h-svh
+                                lg:translate-x-0 lg:size-auto lg:static lg:flex-row lg:bg-transparent lg:text-black lg:text-2xl`}>
+                    <li className='px-4 py-2'>
+                        <Link className='lg:after:block lg:after:w-full lg:after:bg-orange-500 lg:after:h-px lg:after:scale-0 lg:hover:after:scale-100 after:transition-all' to="/" onClick={toggleMenu}>Inicio</Link> 
+                        </li>
+                    <li className='px-4 py-2'>
+                        <Link className='lg:after:block lg:after:w-full lg:after:bg-orange-500 lg:after:h-px lg:after:scale-0 lg:hover:after:scale-100 after:transition-all' to="/carta" onClick={toggleMenu}>Carta</Link> 
+                        </li>
+                    <li className='px-4 py-2'>
+                        <Link className='lg:after:block lg:after:w-full lg:after:bg-orange-500 lg:after:h-px lg:after:scale-0 lg:hover:after:scale-100 after:transition-all' to="/contacto" onClick={toggleMenu}>Contacto</Link> 
+                        </li>
                 </ul>
-                <div onClick={toggleMenu} className="cursor-pointer hb">
+                <div onClick={toggleMenu} className="cursor-pointer hb lg:hidden">
                     <div className={`transition-all duration-300 hb1 ${menu ? 'active' : ''}`}></div>
                     <div className={`transition-all duration-300 hb2 ${menu ? 'active' : ''}`}></div>
                     <div className={`transition-all duration-300 hb3 ${menu ? 'active' : ''}`}></div>

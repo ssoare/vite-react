7a98bf46ce6c365dc2ea0f65b61894eff69f9fa1
ssoare <154654902+ssoare@users.noreply.github.com>
2024-03-20 06:00:36 +0000

    añadir link en el logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,9 @@ function Header() {
     return (
         <header>
             <nav className="fixed top-0 flex items-center justify-between w-full h-12 px-4">
-                <h1 className='flex flex-col italic font-semibold'>El Rincón <span className='text-orange-500'>del Bocata</span></h1>
+                <Link to="/">
+                    <h1 className='flex flex-col italic font-semibold'>El Rincón <span className='text-orange-500'>del Bocata</span></h1>
+                </Link>
                 <ul className={`absolute left-0 top-0 flex flex-col items-center justify-center w-screen gap-4 text-3xl text-orange-500 transition bg-zinc-900 menu ${menu ? '' : 'translate-x-full'} h-svh`}>
                     <li className='px-4 py-2'><Link to="/" onClick={toggleMenu}>Inicio</Link> </li>
                     <li className='px-4 py-2'><Link to="/carta" onClick={toggleMenu}>Carta</Link> </li>

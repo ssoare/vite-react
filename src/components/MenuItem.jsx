ef9b40a4a66a1dbb8b5786bfd179f36111c66859
Soare <soarenexus@gmail.com>
2024-03-22 11:48:35 +0100

    none

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,8 +1,10 @@
-export default function MenuItem({ name = 'Tapas', onClick }) {
-    return (
-        <button className="border-2 py-2 px-4 rounded-full border-orange-500 capitalize mr-2"
-                onClick={() => onClick(name)}>
+import '../css/index.css'
+export default function MenuItem({ name = 'Tapas', click }) {
+    return(
+        <button id = {name} 
+        className="border-2 py-2 px-4 rounded-full capitalize mr-2"
+        onClick={() => click(name)}>
             {name}
         </button>
-    );
+    )
 }
--- src/pages/Carta.jsx
+++ src/pages/Carta.jsx
@@ -1,43 +1,43 @@
-import Tapas from "./submenu/Tapas"
-import Infantil from "./submenu/Infantil"
-import Sandwiches from "./submenu/Sandwiches"
-import Ensaladas from "./submenu/Ensaladas"
-import Bocadillos from "./submenu/Bocadillos"
-import ComidaVegetariana from "./submenu/ComidaVegetariana"
-import Hamburguesas from "./submenu/Hamburguesas"
-import PlatosCombinados from "./submenu/PlatosCombinados"
-import Torraetas from "./submenu/Torraetas"
-import MenuItem from "../components/MenuItem"
-import { useState } from "react"
+    import Tapas from "./submenu/Tapas"
+    import Infantil from "./submenu/Infantil"
+    import Sandwiches from "./submenu/Sandwiches"
+    import Ensaladas from "./submenu/Ensaladas"
+    import Bocadillos from "./submenu/Bocadillos"
+    import ComidaVegetariana from "./submenu/ComidaVegetariana"
+    import Hamburguesas from "./submenu/Hamburguesas"
+    import PlatosCombinados from "./submenu/PlatosCombinados"
+    import Torraetas from "./submenu/Torraetas"
+    import MenuItem from "../components/MenuItem"
+    import { useState } from "react"
 
-export default function Carta() {
-    const [menu, setMenu] = useState('Tapas');
+    export default function Carta() {
+        const [menu, setMenu] = useState('Tapas');
 
-    return(
-        <>
-            <section className="mt-20 mx-4 whitespace-nowrap overflow-auto">
-                <MenuItem name='Tapas' onClick={() => setMenu('Tapas')} />
-                <MenuItem name='Ensaladas' onClick={() => setMenu('Ensaladas')} />
-                <MenuItem name='Torraetas' onClick={() => setMenu('Torraetas')} />
-                <MenuItem name='Sandwiches' onClick={() => setMenu('Sandwiches')} />
-                <MenuItem name='Bocadillos' onClick={() => setMenu('Bocadillos')} />
-                <MenuItem name='Hamburguesas' onClick={() => setMenu('Hamburguesas')} />
-                <MenuItem name='Platos Combinados' onClick={() => setMenu('Platos Combinados')} />
-                <MenuItem name='Infantil' onClick={() => setMenu('Infantil')} />
-                <MenuItem name='Comida Vegetariana' onClick={() => setMenu('Comida Vegetariana')} />
-            </section>
+        return(
+            <>
+                <section id='submenu' className="mt-20 mx-4 whitespace-nowrap overflow-auto">
+                    <MenuItem name='Tapas' click={() => setMenu('Tapas')} />
+                    <MenuItem name='Ensaladas' click={() => setMenu('Ensaladas')} />
+                    <MenuItem name='Torraetas' click={() => setMenu('Torraetas')} />
+                    <MenuItem name='Sandwiches' click={() => setMenu('Sandwiches')} />
+                    <MenuItem name='Bocadillos' click={() => setMenu('Bocadillos')} />
+                    <MenuItem name='Hamburguesas' click={() => setMenu('Hamburguesas')} />
+                    <MenuItem name='Platos Combinados' click={() => setMenu('Platos Combinados')} />
+                    <MenuItem name='Infantil' click={() => setMenu('Infantil')} />
+                    <MenuItem name='Comida Vegetariana' click={() => setMenu('Comida Vegetariana')} />
+                </section>
 
-            <section className="flex flex-wrap justify-center gap-4 mt-8">
-                { menu === 'Tapas' && <Tapas /> }
-                { menu === 'Infantil' && <Infantil /> }
-                { menu === 'Ensaladas' && <Ensaladas /> }
-                { menu === 'Sandwiches' && <Sandwiches /> }
-                { menu === 'Bocadillos' && <Bocadillos /> }
-                { menu === 'Comida Vegetariana' && <ComidaVegetariana /> }
-                { menu === 'Hamburguesas' && <Hamburguesas /> }
-                { menu === 'Platos Combinados' && <PlatosCombinados /> }
-                { menu === 'Torraetas' && <Torraetas /> }
-            </section>
-        </>
-    );
-}
+                <section className="flex flex-wrap justify-center gap-4 mt-8">
+                    { menu === 'Tapas' && <Tapas /> }
+                    { menu === 'Infantil' && <Infantil /> }
+                    { menu === 'Ensaladas' && <Ensaladas /> }
+                    { menu === 'Sandwiches' && <Sandwiches /> }
+                    { menu === 'Bocadillos' && <Bocadillos /> }
+                    { menu === 'Comida Vegetariana' && <ComidaVegetariana /> }
+                    { menu === 'Hamburguesas' && <Hamburguesas /> }
+                    { menu === 'Platos Combinados' && <PlatosCombinados /> }
+                    { menu === 'Torraetas' && <Torraetas /> }
+                </section>
+            </>
+        );
+    }

9a0843dae427e97a2e1b466229ffe50d876a9166
Soare <soarenexus@gmail.com>
2024-03-23 18:37:05 +0100

    restyle button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
-import Hero from "../components/Hero"
+import { Link } from "react-router-dom"
 
-function Home() {
+export default function Home() {
     return(
-        <Hero />
+        <>
+            <section className="hero text-4xl flex flex-col items-center justify-center gap-8 mt-12">
+                <h1>El Rincón del Bocata</h1>
+                
+                <Link to='contacto'>
+                    <button className='text-white text-xl bg-black/50 py-2 px-8 font-semibold rounded-xl border-2
+                                hover:bg-orange-500 hover:text-black transition'>
+                        RESERVA
+                    </button>
+                </Link>
+                
+            </section>
+        </>
     )
 }
-
-export default Home
